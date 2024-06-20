59d5b9183c1b6df3a3cfefd59d744568e5c13f87
João Victor da Silva Sales <56794256+VictorSilvaaa@users.noreply.github.com>
2024-06-20 11:52:36 -0300

    feat: add relacionamento one-to-one

--- curso-laravel-8-relacionamentos-tabelas/routes/web.php
+++ curso-laravel-8-relacionamentos-tabelas/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\{
+    User,
+    Preference
+};
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +20,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/one-to-one', function(){
+    $user = User::first();
+
+    $data = [
+        'background_color' => '#000',
+
+    ];
+    if($user->preference){
+        $user->preference->update($data);
+    }else{
+        $user->preference()->create($data);
+    }
+   
+
+    $user->refresh();
+
+    dd($user->preference);
 });
